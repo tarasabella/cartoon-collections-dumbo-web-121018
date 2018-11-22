47d94285c42469a28eb340a2c031443119fe82ee
Tara Sabella <tarasabella@gmail.com>
2018-11-22 03:06:26 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,8 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
-end
+def roll_call_dwarves(dwarves)
+   dwarves.each_with_index do |dwarves, index|
+  puts "#{index + 1}. #{dwarves}"
+end 
+
 
 def summon_captain_planet# code an argument here
   # Your code here
